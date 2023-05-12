e1e1a09d8b7b5e9648a5d76d87fbc1ad6b0bc83f
Gonzalo Rubino <gonzalorubino@gmail.com>
2023-05-11 23:59:33 -0300

    Add exercise 2 and some retouches

---
+++ js/ejercicio-2.js
@@ -0,0 +1,34 @@
+const API_KEY = "L5dPmbGX3Q8bgBNwcWThdRxrnCXhpPyP";
+const elementoBoton = document.getElementById('sendButton');
+const elementoPhrase = document.getElementById('phrase');
+const elementoImg = document.getElementById('image'); 
+
+// Uso de Promise All
+// uso una function async
+const fetchNames = async () => {
+    // Genero un numero random para el offset, entonces obtengo resultados distintos en cada fetch
+    const random = Math.floor(Math.random() * 100);
+
+
+    try {
+      // Promise all, va a esperar que se ejecuten un array de promesas (https://es.javascript.info/promise-api)
+      const response = await Promise.all([
+        fetch('https://api.chucknorris.io/jokes/random'),
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q='Chuck Norris'&limit=1&offset=${random}&rating=g&lang=en`)
+      ]);
+
+      // Espero que se ejecuten ambas promises con un await
+      // Cuando esten listas, van a ejecutar un map para juntar la respuesta en un array
+      const data = await Promise.all(response.map(resp => resp.json()));
+
+      // Imprimo lo que me vino desde la API
+      // data[0] es la posicion 0 del array, y van a venir los datos del primer fetch
+      // data[1] es la posicion 1 del array, y van a venir los datos del segundo fetch
+      elementoPhrase.innerHTML = `<p><b>El gran Chuck Norris dijo:</b></p><p>${data[0].value}</p>`;
+      elementoImg.innerHTML = `<img src="${data[1].data[0].images.downsized.url}" />`;
+    } catch {
+      throw Error("Promise failed");
+    }
+};
+
+elementoBoton.addEventListener('click', fetchNames);
--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,14 +1,16 @@
-const API_KEY = "AZCjM68y1GkgBjzTFlISEwbwQGJqPdjZ";
 const elementoBoton = document.getElementById('sendButton');
 const elementoPhrase = document.getElementById('phrase');
 const elementoTranslated = document.getElementById('translated-phrase');
-const elementoImg = document.getElementById('image'); 
 
 const buscarFrase = ()=>{
-    elementoPhrase.innerHTML = `<div class="spinner-border" role="status">
+    // Creo un loader
+    elementoPhrase.innerHTML = `
+    <div class="spinner-border text-danger" role="status">
         <span class="sr-only">Cargando frase...</span>
-    </div>`;
+    </div>
+    <p class="mt-2 text-danger">Cargando frase...</p>`;
 
+    // Primer Fetch
     fetch('https://api.chucknorris.io/jokes/random')
     .then(function(response){        
         return response.json();
@@ -16,10 +18,17 @@ const buscarFrase = ()=>{
         elementoPhrase.innerHTML = `<p><b>El gran Chuck Norris dijo:</b></p><p>${json.value}</p>`;
         return json.value;
     }).then(async function(frase){
-        elementoTranslated.innerHTML =`<div class="spinner-border" role="status">
-            <span class="sr-only">Cargando traduccion...</span>
-        </div>`;
+        // Uso de fetch con una funcion async 
 
+        // Creo un loader
+        elementoTranslated.innerHTML =`
+        <div class="spinner-border text-success" role="status">
+            <span class="sr-only">Cargando traduccion...</span>
+        </div>
+        <p class="mt-2 text-success">Traduciendo...</p>`;
+
+        // Segundo Fetch
+        // Uso lo que me vino del primer fetch
         const res = await fetch("https://translate.argosopentech.com/translate", {
             method: "POST",
             body: JSON.stringify({
@@ -34,33 +43,12 @@ const buscarFrase = ()=>{
         elementoTranslated.innerHTML = text.translatedText;
     })
     .finally(function(){
-        console.log('finalice la primer parte')
+        console.log('Me ejecuto al final siempre')
     })
     .catch(function(err){
         console.log("Something went wrong", err);
     });
 }
 
-const buscarImagen = (author) => {
-    fetch("https://api.giphy.com/v1/gifs/search?api_key="+ API_KEY +"&q="+ author +"&limit=1&offset=0&rating=g&lang=en")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log('data img - linea 33: ', data);
-        elementoImg.innerHTML = `<img src="${data.data[0].images.downsized.url}" />`;
-    })
-    .catch(function(err){console.log('error!: ',err)})
-}
-
+// Agrego listener al boton
 elementoBoton.addEventListener('click', buscarFrase);
-
-function imprimirResultado(data){
-    const resultDiv = document.getElementById('main');
-    let images = '';
-
-
-    // images += `<div><img src="${data.images.downsized.url}" height="${data.images.downsized.height * 2}"/><div>`
-    images += `<a href="${data.bitly_gif_url}">${data.id}</a>`
-    resultDiv.innerHTML = `<div class='row'>${images}</div>`;
-}
